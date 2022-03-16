e28a84a4e024d0c0e20e1e905262bff62d7dcc9f
Samson Main <Samson Main>
2022-03-16 13:28:48 +0000

    Add functionality

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -11,13 +11,28 @@ import uk.ac.bris.cs.scotlandyard.model.Board;
 import uk.ac.bris.cs.scotlandyard.model.Move;
 
 public class MyAi implements Ai {
-
 	@Nonnull @Override public String name() { return "D:"; }
 
 	@Nonnull @Override public Move pickMove(
 			@Nonnull Board board,
 			Pair<Long, TimeUnit> timeoutPair) {
-		Dijkstra dijkstra = new Dijkstra(board.getSetup().graph);
+		if (mrXToMove(board)) {
+			return pickMrXMove(board);
+		} else {
+			return pickDetectiveMove(board);
+		}
+	}
+
+	private boolean mrXToMove(Board board) {
+		return board.getAvailableMoves().stream()
+				.anyMatch(move -> move.commencedBy().isMrX());
+	}
+
+	private Move pickMrXMove(Board board) {
+		return null;
+	}
+
+	private Move pickDetectiveMove(Board board) {
 		// returns a random move, replace with your own implementation
 		var moves = board.getAvailableMoves().asList();
 		return moves.get(new Random().nextInt(moves.size()));
