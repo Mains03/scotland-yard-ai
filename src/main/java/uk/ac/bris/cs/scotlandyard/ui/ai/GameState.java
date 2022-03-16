601de33016c00a9f405ad71c557f8acbece6ac76
Samson Main <Samson Main>
2022-03-16 13:36:28 +0000

    Add functionality

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/GameState.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/GameState.java
@@ -2,24 +2,45 @@ package uk.ac.bris.cs.scotlandyard.ui.ai;
 
 import uk.ac.bris.cs.scotlandyard.model.Board;
 import uk.ac.bris.cs.scotlandyard.model.Move;
+import uk.ac.bris.cs.scotlandyard.model.Player;
 
+import java.util.Collection;
 import java.util.Objects;
 
 public interface GameState {
-    public Move pickMove();
+    Move pickMove();
 
-    public class MrXMoveGameState implements GameState {
+    class MrXMoveGameState implements GameState {
+        private final Player mrX;
+        private final Collection<Player> detectives;
+        private final Move move;
+
         public MrXMoveGameState(Board board) {
             Objects.requireNonNull(board);
+            mrX = createMrX(board);
+            detectives = createDetectives();
+            move = createBestMove();
+        }
+
+        private Player createMrX(Board board) {
+            return null;
+        }
+
+        private Collection<Player> createDetectives() {
+            return null;
+        }
+
+        private Move createBestMove() {
+            return null;
         }
 
         @Override
         public Move pickMove() {
-            return null;
+            return move;
         }
     }
 
-    public class DetectiveMoveGameState implements GameState {
+    class DetectiveMoveGameState implements GameState {
         @Override
         public Move pickMove() {
             return null;
