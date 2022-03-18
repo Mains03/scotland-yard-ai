d4ff9b0a993f4b64001562b2046a946cb688ef9c
Mains03 <samsonsmain@gmail.com>
2022-03-18 14:21:55 +0000

    Revert "Make constructor public"

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/GameTree.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/GameTree.java
@@ -1,56 +1,36 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
 import uk.ac.bris.cs.scotlandyard.model.Board;
-import uk.ac.bris.cs.scotlandyard.model.Move;
 
-import java.util.*;
-import java.util.function.ToIntFunction;
+import java.util.Collection;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public final class GameTree {
     private final Board.GameState gameState;
-    private final Optional<Move> moveMade;
 
-    private List<GameTree> children;
-    private Optional<Move> evaluation;
-
-    public GameTree(final Board.GameState gameState, final Optional<Move> moveMade) {
+    private GameTree(final Board.GameState gameState) {
         Objects.requireNonNull(gameState);
         this.gameState = gameState;
-        this.moveMade = moveMade;
-        children = new ArrayList<>();
     }
 
-    public Stream<GameTree> generateTree() {
-        return generateTree(this);
+    public Stream<GameTree> generate() {
+        return generate(this);
     }
 
-    private Stream<GameTree> generateTree(GameTree node) {
-        createChildren();
-        return children.stream()
-                .flatMap(this::generateTree);
+    private Stream<GameTree> generate(GameTree node) {
+        return Stream.concat(
+                Stream.of(node),
+                createChildren().stream().flatMap(this::generate)
+        );
     }
 
-    private void createChildren() {
-        children = gameState.getAvailableMoves().stream()
+    private Collection<GameTree> createChildren() {
+        return gameState.getAvailableMoves().stream()
                 .map(move -> new GameTree(gameState.advance(move)))
                 .collect(Collectors.toList());
     }
 
-    public Move evaluate(ToIntFunction<Board.GameState> staticEvaluationStrategy) {
-        if (evaluation.isPresent()) return evaluation.get();
-        else {
-            evaluation = children.stream()
-                    .map(child -> evaluate(staticEvaluationStrategy))
-                    .max(Integer::compareTo);
-            if (evaluation.isPresent()) {
-                // contains best value for children
-                return evaluation.get();
-            } else {
-                // no children
-                return staticEvaluationStrategy.applyAsInt(gameState);
-            }
-        }
-    }
+    public Board.GameState getGameState() { return gameState; }
 }
