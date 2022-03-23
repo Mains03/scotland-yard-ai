93b8d3fbac46342b1121f9258742774a90cb50d5
Samson Main <Samson Main>
2022-03-23 12:58:27 +0000

    Refactor move generation

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/moves/AllMoves.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/moves/AllMoves.java
@@ -1,73 +1,49 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai.moves;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.graph.ImmutableValueGraph;
+import uk.ac.bris.cs.scotlandyard.model.Move;
 import uk.ac.bris.cs.scotlandyard.model.Piece;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard;
 
 import java.util.*;
 
 /**
- * Singleton class which precomputes all possible moves at every location
- * for each player.
+ * Singleton class to determine the available (not necessarily possible) moves
+ * for each player at each location.
  */
 public class AllMoves {
+    // AllMoves singleton instance
     private static AllMoves allMoves;
 
     /**
-     * Creates an instance of the AllMoves singleton class.
-     * @param graph the board
+     * Creates the AllMoves singleton instance
+     * @param graph board game
      */
     public static void createInstance(ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph) {
-        Objects.requireNonNull(graph);
         if (allMoves == null)
             allMoves = new AllMoves(graph);
     }
 
     /**
-     * @return the AllMoves singleton instance.
+     *
+     * @return AllMoves singleton instance
      */
     public static AllMoves getInstance() {
         Objects.requireNonNull(allMoves);
         return allMoves;
     }
 
-    /**
-     * The available moves to each player at each location.
-     */
-    private final ImmutableMap<Piece, PlayerMoves> moves;
+    private final PlayerMovesFactory playerMovesFactory;
 
-    private AllMoves(ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph) {
-        moves = createMoves(graph);
-    }
+    // moves available to a player
+    private Map<Piece, PlayerMoves> moves;
 
-    /**
-     * Creates all the moves available to each player at each location.
-     * @param graph game board
-     * @return
-     */
-    private ImmutableMap<Piece, PlayerMoves> createMoves(
+    private AllMoves(
             ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph
     ) {
-        Map<Piece, PlayerMoves> moves = new HashMap<>();
-        for (Piece piece : getPieces())
-            moves.put(piece, new PlayerMoves(graph, piece));
-        return ImmutableMap.copyOf(moves);
-    }
-
-    /**
-     * @return iterable collection of all the pieces.
-     */
-    private Iterable<Piece> getPieces() {
-        return Arrays.asList(
-                Piece.MrX.MRX,
-                Piece.Detective.BLUE,
-                Piece.Detective.GREEN,
-                Piece.Detective.RED,
-                Piece.Detective.WHITE,
-                Piece.Detective.YELLOW
-        );
+        this.playerMovesFactory = new PlayerMovesFactory(graph);
+        moves = new HashMap<>();
     }
 
     /**
@@ -76,7 +52,13 @@ public class AllMoves {
      * @param location location of the piece
      * @return available moves to the piece at the location
      */
-    public MoveCollection getAvailableMoves(final Piece piece, int location) {
-        return moves.get(piece).getMoves(location);
+    public Collection<Move> getAvailableMoves(final Piece piece, int location) {
+        if (!moves.containsKey(piece))
+            moves.put(piece, createPlayerMoves(piece));
+        return moves.get(piece).getAvailableMoves(location);
+    }
+
+    private PlayerMoves createPlayerMoves(final Piece piece) {
+        return playerMovesFactory.createPlayerMoves(piece);
     }
 }
