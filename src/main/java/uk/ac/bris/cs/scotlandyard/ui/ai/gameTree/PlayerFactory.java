044daa2517ce3f4b825ba89304ba45397e9f5be5
Mains03 <samsonsmain@gmail.com>
2022-03-23 19:30:13 +0000

    Implement PlayerFactory

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/gameTree/PlayerFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/gameTree/PlayerFactory.java
@@ -1,10 +1,13 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai.gameTree;
 
-import uk.ac.bris.cs.scotlandyard.model.Board;
-import uk.ac.bris.cs.scotlandyard.model.Move;
-import uk.ac.bris.cs.scotlandyard.model.Player;
+import com.google.common.collect.ImmutableMap;
+import uk.ac.bris.cs.scotlandyard.model.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 class PlayerFactory {
     private static PlayerFactory playerFactory;
@@ -18,10 +21,58 @@ class PlayerFactory {
     private PlayerFactory() {}
 
     Player createMrX(final Board board, final Move move) {
-        return null;
+        return new Player(
+                Piece.MrX.MRX,
+                createPlayerTickets(board, Piece.MrX.MRX),
+                move.source()
+        ).use(move.tickets()).at(getMoveDestination(move));
+    }
+
+    private int getMoveDestination(final Move move) {
+        return move.accept(new Move.Visitor<>() {
+            @Override
+            public Integer visit(Move.SingleMove move) {
+                return move.destination;
+            }
+
+            @Override
+            public Integer visit(Move.DoubleMove move) {
+                return move.destination2;
+            }
+        });
     }
 
     List<Player> createDetectives(final Board board) {
-        return null;
+        return board.getPlayers().stream()
+                .filter(Piece::isDetective)
+                .map(piece -> createDetective(board, (Piece.Detective) piece))
+                .flatMap(Optional::stream)
+                .collect(Collectors.toList());
+    }
+
+    private Optional<Player> createDetective(final Board board, final Piece.Detective piece) {
+        Optional<Integer> location = board.getDetectiveLocation(piece);
+        if (location.isEmpty()) return Optional.empty();
+        return Optional.of(new Player(
+                piece,
+                createPlayerTickets(board, piece),
+                location.get()
+        ));
+    }
+
+    private ImmutableMap<ScotlandYard.Ticket, Integer> createPlayerTickets(
+            final Board board,
+            final Piece piece
+    ) {
+        Map<ScotlandYard.Ticket, Integer> tickets = new HashMap<>();
+        Optional<Board.TicketBoard> ticketBoard = board.getPlayerTickets(piece);
+        if (ticketBoard.isPresent()) {
+            for (ScotlandYard.Ticket ticket : ScotlandYard.Ticket.values()) {
+                int count = ticketBoard.get().getCount(ticket);
+
+                tickets.put(ticket, count);
+            }
+        }
+        return ImmutableMap.copyOf(tickets);
     }
 }
