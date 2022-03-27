742faedb9afcacc37761880fdafa09a7d66a970a
Mains03 <samsonsmain@gmail.com>
2022-03-27 17:21:19 +0100

    Refactor GameState

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/gameTree/GameState.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/gameTree/GameState.java
@@ -1,8 +1,12 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai.gameTree;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.graph.ImmutableValueGraph;
 import uk.ac.bris.cs.scotlandyard.model.Move;
+import uk.ac.bris.cs.scotlandyard.model.Piece;
 import uk.ac.bris.cs.scotlandyard.model.Player;
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard;
 import uk.ac.bris.cs.scotlandyard.ui.ai.MinimumDistance;
 
 import java.util.*;
@@ -11,12 +15,12 @@ import java.util.*;
  * A state of the game, either MrX to move or all detectives to move.
  */
 class GameState {
+    private final ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph;
     private final Player mrX;
     private final List<Player> detectives;
-
     private final boolean mrXTurn;
-
-    private final Move move;
+    private final List<Move> moves; // moves made to get to this state of the game
+    private final List<Move> availableMoves;
 
     /**
      * GameState constructor
@@ -24,15 +28,30 @@ class GameState {
      * @param detectives detectives
      * @param mrXTurn whether MrX is to move
      */
-    GameState(final Player mrX, final List<Player> detectives, final boolean mrXTurn, final Move move) {
+    GameState(
+            final ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph,
+            final Player mrX,
+            final List<Player> detectives,
+            final boolean mrXTurn,
+            final List<Move> moves
+    ) {
+        Objects.requireNonNull(graph);
         Objects.requireNonNull(mrX);
         Objects.requireNonNull(detectives);
+        Objects.requireNonNull(moves);
+        this.graph = graph;
         this.mrX = mrX;
         this.detectives = detectives;
         this.mrXTurn = mrXTurn;
-        this.move = move;
+        this.moves = moves;
+        this.availableMoves = generateAvailableMoves();
     }
 
+    private List<Move> generateAvailableMoves() {
+        return null;
+    }
+
+    ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> getGraph() { return graph; }
     Player getMrX() { return mrX; }
     ImmutableList<Player> getDetectives() { return ImmutableList.copyOf(detectives); }
     boolean isMrXTurn() { return mrXTurn; }
@@ -50,7 +69,11 @@ class GameState {
             return 10000000;
     }
 
-    Move getMove() {
-        return move;
+    List<Move> getMoves() {
+        return moves;
+    }
+
+    List<Move> getAvailableMoves() {
+        return null;
     }
 }
--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/gameTree/GameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/gameTree/GameStateFactory.java
@@ -1,10 +1,13 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai.gameTree;
 
+import io.atlassian.fugue.Pair;
 import uk.ac.bris.cs.scotlandyard.model.Board;
 import uk.ac.bris.cs.scotlandyard.model.Move;
+import uk.ac.bris.cs.scotlandyard.model.Piece;
 import uk.ac.bris.cs.scotlandyard.model.Player;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Singleton class used to create game states.
@@ -27,7 +30,7 @@ class GameStateFactory {
     GameState createGameState(final Board board, final Move mrXMove) {
         Player mrX = PlayerFactory.getInstance().createMrX(board, mrXMove);
         List<Player> detectives = PlayerFactory.getInstance().createDetectives(board);
-        return new GameState(mrX, detectives, false, mrXMove);
+        return new GameState(board.getSetup().graph, mrX, detectives, false, List.of(mrXMove));
     }
 
     /**
@@ -35,43 +38,125 @@ class GameStateFactory {
      * @param gameState current game state
      * @return possible game states for the next turn
      */
-    Collection<GameState> nextGameStates(final GameState gameState) {
-        if (gameState.isMrXTurn())
-            return nextGameStatesMrXTurn(gameState);
-        else
-            return nextGameStatesDetectivesTurn(gameState);
-    }
-
-    /**
-     * Generates all the game states where MrX is to move.
-     * @param gameState current game state
-     * @return possible game states with detectives to move
-     */
-    private Collection<GameState> nextGameStatesMrXTurn(final GameState gameState) {
-        if (!gameState.isMrXTurn()) throw new IllegalArgumentException();
-        Collection<GameState> nextGameStates = new ArrayList<>();
-        for (Move move : MoveGenerationFactory.getInstance().generateMrXMoves(gameState)) {
-            Player mrX = PlayerFactory.getInstance().moveMrX(gameState.getMrX(), move);
-            List<Player> detectives = gameState.getDetectives();
-            nextGameStates.add(new GameState(mrX, detectives, false, move));
+    List<GameState> nextGameStates(final GameState gameState) {
+        if (gameState.isMrXTurn()) {
+            List<List<Move>> moveList = List.of(gameState.getAvailableMoves());
+            return constructGameStates(gameState, moveList);
+        } else {
+            Map<Piece, List<Move>> availableMoves = new HashMap<>();
+            for (Move move : gameState.getAvailableMoves()) {
+                if (availableMoves.containsKey(move.commencedBy()))
+                    availableMoves.get(move.commencedBy()).add(move);
+                else
+                    availableMoves.put(move.commencedBy(), new ArrayList<>(List.of(move)));
+            }
+            List<List<Move>> moveList = createDetectivesMoveList(
+                    gameState.getDetectives().stream().map(Player::piece).collect(Collectors.toList()),
+                    availableMoves,
+                    new ArrayList<>()
+            );
+            return constructGameStates(gameState, moveList);
         }
-        return nextGameStates;
     }
 
     /**
-     * Generates all the game states where the detectives are to move.
-     * @param gameState current game state
-     * @return possible game states with MrX to move
+     * Recursively generates all combinations of the detective moves.
+     * @param remaining detectives yet to move
+     * @param currentMoveList move list of detectives who have moved
+     * @return move list
      */
-    private Collection<GameState> nextGameStatesDetectivesTurn(final GameState gameState) {
-        Collection<GameState> nextGameStates = new ArrayList<>();
-        List<Map<Player, Move>> allDetectiveMoves = MoveGenerationFactory.getInstance().generateDetectivesMoves(gameState);
-        for (Map<Player, Move> detectiveMoves : allDetectiveMoves) {
+    private List<List<Move>> createDetectivesMoveList(
+            final List<Piece> remaining,
+            final Map<Piece, List<Move>> availableMoves,
+            final List<List<Move>> currentMoveList
+    ) {
+        for (Piece piece : remaining) {
+            if (currentMoveList.size() == 0) {
+                for (Move move : availableMoves.get(piece))
+                    currentMoveList.add(new ArrayList<>(List.of(move)));
+            } else {
+                List<List<Move>> newMoveList = new ArrayList<>();
+                for (Move move : availableMoves.get(piece)) {
+                    for (List<Move> moveList : currentMoveList) {
+                        if (!hasSameDestination(moveList, move)) {
+                            newMoveList.add(moveList);
+                            newMoveList.get(newMoveList.size()-1).add(move);
+                        }
+                    }
+                }
+                List<Piece> newRemaining = List.copyOf(remaining);
+                newRemaining.remove(piece);
+                currentMoveList.addAll(createDetectivesMoveList(newRemaining, availableMoves, newMoveList));
+            }
+        }
+        return currentMoveList;
+    }
+
+    /**
+     * Checks if the given move has the same destination as a move in the move list.
+     * @param moveList the move list
+     * @param move the new move
+     * @return whether the same destination is present
+     */
+    private boolean hasSameDestination(final List<Move> moveList, final Move move) {
+        int moveDestination = moveDestination(move);
+        for (Move moveListMove : moveList) {
+            if (moveDestination(moveListMove) == moveDestination)
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * Creates the game state associated to each list of moves.
+     * @param gameState the game state
+     * @param moveList the list of moves for players to make
+     * @return the new game states
+     */
+    private List<GameState> constructGameStates(final GameState gameState, final List<List<Move>> moveList) {
+        List<GameState> gameStates = new ArrayList<>();
+        for (List<Move> moves : moveList) {
             Player mrX = gameState.getMrX();
-            List<Player> detectives = PlayerFactory.getInstance()
-                    .moveDetectives(gameState.getDetectives(), detectiveMoves);
-            nextGameStates.add(new GameState(mrX, detectives, true, null));
+            List<Player> detectives = gameState.getDetectives();
+            for (Move move : moves) {
+                int destination = moveDestination(move);
+                if (move.commencedBy().isMrX())
+                    mrX = mrX.use(move.tickets()).at(destination);
+                else {
+                    mrX = mrX.give(move.tickets());
+                    for (int i=0; i<detectives.size(); ++i) {
+                        if (detectives.get(i).piece() == move.commencedBy())
+                            detectives.set(i, detectives.get(i).use(move.tickets()).at(destination));
+                    }
+                }
+            }
+            gameStates.add(new GameState(
+                    gameState.getGraph(),
+                    mrX,
+                    detectives,
+                    !gameState.isMrXTurn(),
+                    moves
+            ));
         }
-        return nextGameStates;
+        return gameStates;
+    }
+
+    /**
+     * Destination of a move.
+     * @param move the move
+     * @return the destination
+     */
+    private int moveDestination(final Move move) {
+        return move.accept(new Move.Visitor<>() {
+            @Override
+            public Integer visit(Move.SingleMove move) {
+                return move.destination;
+            }
+
+            @Override
+            public Integer visit(Move.DoubleMove move) {
+                return move.destination2;
+            }
+        });
     }
 }
