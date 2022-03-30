1448089f0bb9f577e0bfb4443b50419f2e0c203c
Mains03 <samsonsmain@gmail.com>
2022-03-30 17:13:33 +0100

    Create MinimumDistanceStaticPositionEvaluation

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/staticPositionEvaluationStrategy/MinimumDistanceStaticPositionEvaluation.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/staticPositionEvaluationStrategy/MinimumDistanceStaticPositionEvaluation.java
@@ -1,10 +1,30 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai.staticPositionEvaluationStrategy;
 
-public class MinimumDistanceStaticPositionEvaluation implements StaticPositionEvaluationStrategy {
+import uk.ac.bris.cs.scotlandyard.model.Player;
+import uk.ac.bris.cs.scotlandyard.ui.ai.minimumDistanceAlgorithm.MinimumDistanceAlgorithmStrategy;
 
+import java.util.List;
+import java.util.Objects;
+
+public class MinimumDistanceStaticPositionEvaluation implements StaticPositionEvaluationStrategy {
+    private static final int POSITIVE_INFINITY = 10000000;
+
+    private final MinimumDistanceAlgorithmStrategy algorithm;
+
+    public MinimumDistanceStaticPositionEvaluation(MinimumDistanceAlgorithmStrategy algorithm) {
+        Objects.requireNonNull(algorithm);
+        this.algorithm = algorithm;
+    }
 
     @Override
     public int evaluate(AiGameState gameState) {
-
+        Player mrX = gameState.getMrX();
+        List<Player> detectives = gameState.getDetectives();
+        int minDist = POSITIVE_INFINITY;
+        for (Player detective : detectives) {
+            int dist = algorithm.minimumDistance(detective, mrX);
+            minDist = Math.min(minDist, dist);
+        }
+        return minDist;
     }
 }
