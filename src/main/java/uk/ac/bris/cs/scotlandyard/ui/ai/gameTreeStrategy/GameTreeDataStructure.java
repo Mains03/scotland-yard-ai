2364fd0cf9e7b21e8cdd7a1f160122e94f5a945e
Mains03 <samsonsmain@gmail.com>
2022-04-01 15:36:58 +0100

    Create methods for GameTreeDataStructure

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/gameTreeStrategy/GameTreeDataStructure.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/gameTreeStrategy/GameTreeDataStructure.java
@@ -1,4 +1,13 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai.gameTreeStrategy;
 
+import uk.ac.bris.cs.scotlandyard.model.Player;
+
+import java.util.List;
+
 public interface GameTreeDataStructure {
+    Player getMrX();
+
+    List<Player> getDetectives();
+
+    List<GameTreeDataStructure> getChildren();
 }
