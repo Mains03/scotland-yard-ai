52f0f0cb73f915640073e7e78a9fe01eb13364ac
Mains03 <samsonsmain@gmail.com>
2022-04-14 17:38:44 +0100

    Undo changes to PiecePlayerFactoryAdapter

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/minimumDistanceStrategy/aiBoard/PiecePlayerFactoryAdapter.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/minimumDistanceStrategy/aiBoard/PiecePlayerFactoryAdapter.java
@@ -1,8 +1,7 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai.minimumDistanceStrategy.aiBoard;
 
+import com.google.common.collect.ImmutableMap;
 import uk.ac.bris.cs.scotlandyard.model.*;
-import uk.ac.bris.cs.scotlandyard.ui.ai.playerFactory.PlayerFactory;
-import uk.ac.bris.cs.scotlandyard.ui.ai.playerFactory.PlayerFactoryAdapterV2;
 
 import java.util.NoSuchElementException;
 import java.util.Objects;
@@ -17,12 +16,57 @@ public class PiecePlayerFactoryAdapter implements PiecePlayerFactory {
 
     @Override
     public Optional<Player> createPlayer(Piece piece) {
-        try {
-            PlayerFactory playerFactory = new PlayerFactoryAdapterV2();
-            Player player = playerFactory.createFromPiece(board, piece);
-            return Optional.of(player);
-        } catch (NoSuchElementException e) {
+        Optional<Player> player;
+        if (piece.isMrX())
+            player = createMrX(piece);
+        else
+            player = createDetective((Piece.Detective) piece);
+        return player;
+    }
+
+    private Optional<Player> createMrX(Piece piece) {
+        // MrX to move
+        // Find any MrX move, the source is MrX's location
+        Optional<Integer> location = board.getAvailableMoves().stream()
+                .filter(move -> move.commencedBy().isMrX())
+                .findAny().stream()
+                .map(Move::source)
+                .findAny();
+        if (location.isEmpty())
             return Optional.empty();
+        else {
+            Player mrX = new Player(
+                    Piece.MrX.MRX,
+                    createPlayerTickets(board, Piece.MrX.MRX),
+                    location.get()
+            );
+            return Optional.of(mrX);
         }
+    }
+
+    private Optional<Player> createDetective(Piece.Detective piece) {
+        Optional<Integer> location = board.getDetectiveLocation(piece);
+        if (location.isEmpty())
+            return Optional.empty();
+        else {
+            Player detective = new Player(
+                    piece,
+                    createPlayerTickets(board, piece),
+                    location.get()
+            );
+            return Optional.of(detective);
+        }
+    }
+
+    private ImmutableMap<ScotlandYard.Ticket, Integer> createPlayerTickets(Board board, Piece piece) {
+        ImmutableMap.Builder<ScotlandYard.Ticket, Integer> tickets = ImmutableMap.builder();
+        Optional<Board.TicketBoard> ticketBoard = board.getPlayerTickets(piece);
+        if (ticketBoard.isEmpty())
+            throw new NoSuchElementException("Piece not found");
+        else {
+            for (ScotlandYard.Ticket ticket : ScotlandYard.Ticket.values())
+                tickets.put(ticket, ticketBoard.get().getCount(ticket));
+        }
+        return tickets.build();
     }
 }
