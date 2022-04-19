fa46d408bc48de7403dc90432f220a3472667dd2
Mains03 <samsonsmain@gmail.com>
2022-04-19 17:11:05 +0100

    Refactor StandardMoveGenerationFactory

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/moveGeneration/StandardMoveGenerationFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/moveGeneration/StandardMoveGenerationFactory.java
@@ -1,12 +1,9 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai.moveGeneration;
 
 import com.google.common.collect.ImmutableSet;
-import com.google.common.graph.ImmutableValueGraph;
 import uk.ac.bris.cs.scotlandyard.model.Move;
 import uk.ac.bris.cs.scotlandyard.model.Player;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard;
-import uk.ac.bris.cs.scotlandyard.ui.ai.minimumDistanceStrategy.aiMove.AiMove;
-import uk.ac.bris.cs.scotlandyard.ui.ai.minimumDistanceStrategy.aiMove.AiMoveAdapter;
 import uk.ac.bris.cs.scotlandyard.ui.ai.minimumDistanceStrategy.aiPlayer.MoveApplyFactory;
 
 import java.util.HashSet;
@@ -15,7 +12,6 @@ import java.util.Set;
 
 /**
  * Singleton class to generate moves for a player.
- * If player has double move tickets, generates double moves as well.
  */
 public class StandardMoveGenerationFactory implements MoveGenerationFactory {
     private static StandardMoveGenerationFactory moveGenerationFactory;
@@ -30,66 +26,118 @@ public class StandardMoveGenerationFactory implements MoveGenerationFactory {
 
     @Override
     public Set<Move> generateMoves(MoveGenerationBoard board, Player player) {
-        if (player.isMrX()) {
-            // If MrX, must generate single and double moves
-            Set<Move> moves = generateSingleMoves(board, player);
-            if (player.has(ScotlandYard.Ticket.DOUBLE)) {
-                // If MrX has a double ticket, check all subsequent moves from each possible single move
-                MoveApplyFactory moveApplyFactory = MoveApplyFactory.getInstance();
-                for (Move firstMove : moves) {
-                    // Apply first move and generate double moves
-                    Player newPlayer = moveApplyFactory.applyMove(player, firstMove);
-                    Set<Move> secondMoves = generateSingleMoves(board, newPlayer);
-                    for (Move secondMove : secondMoves) {
-                        // Checks if destination of second move is source of first
-                        AiMove aiSecondMove = new AiMoveAdapter(secondMove);
-                        if (aiSecondMove.getDestination() != firstMove.source()) {
-                            // Cast first and second moves into SingleMoves then make and add DoubleMove
-                            Move.SingleMove firstMoveSingle = (Move.SingleMove) firstMove;
-                            Move.SingleMove secondMoveSingle = (Move.SingleMove) secondMove;
-                            Move.DoubleMove doubleMove = new Move.DoubleMove(
-                                    player.piece(),
-                                    player.location(),
-                                    firstMoveSingle.ticket,
-                                    firstMoveSingle.destination,
-                                    secondMoveSingle.ticket,
-                                    secondMoveSingle.destination
-                            );
-                            moves.add(doubleMove);
-                        }
-                    }
-                }
-            }
-            return moves;
-        } else
-            // If not MrX, just generate single moves
-            return generateSingleMoves(board, player);
+        Set<Move> moves;
+        // distinguish between MrX and detectives since their moves differ
+        if (player.isMrX())
+            moves = generateMrXMoves(board, player);
+        else
+            moves = generateSingleMoves(board, player);
+        return moves;
     }
 
     private Set<Move> generateSingleMoves(MoveGenerationBoard board, Player player) {
         Set<Move> moves = new HashSet<>();
-        ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph = board.getGraph();
-        int location = player.location();
-        // For each node connected to player location, if transport exists check if player has the ticket
-        for (int destination : graph.adjacentNodes(location)) {
-            Optional<ImmutableSet<ScotlandYard.Transport>> mTransports = graph.edgeValue(location, destination);
-            if (mTransports.isPresent()) {
-                ImmutableSet<ScotlandYard.Transport> transports = mTransports.get();
-                for (ScotlandYard.Transport transport : transports) {
-                    ScotlandYard.Ticket ticket = transport.requiredTicket();
-                    // If transport to this location exists and the player has a ticket
-                    if (player.has(transport.requiredTicket())) {
-                        Move move = new Move.SingleMove(player.piece(), location, ticket, destination);
-                        moves.add(move);
-                    }
-                }
-                // If the player has a secret ticket i.e. they're MrX, add that ticket as well
-                if (player.has(ScotlandYard.Ticket.SECRET)) {
-                    Move move = new Move.SingleMove(player.piece(), location, ScotlandYard.Ticket.SECRET, destination);
+        // destination of moves must not conflict with a detective's location
+        for (int destination : getUnoccupiedAdjacentNodes(board, player)) {
+            Set<ScotlandYard.Transport> transports = getTransports(board, player, destination);
+            for (ScotlandYard.Transport transport : transports) {
+                if (player.has(transport.requiredTicket())) {
+                    Move move = createSingleMove(player, transport.requiredTicket(), destination);
                     moves.add(move);
                 }
             }
+            if (player.has(ScotlandYard.Ticket.SECRET)) {
+                Move move = createSingleMoveWithSecretTicket(player, destination);
+                moves.add(move);
+            }
         }
         return moves;
+    }
+
+    /**
+     * Returns adjacent locations not occupied by a detective.
+     * @param board board
+     * @param player player
+     * @return destinations
+     */
+    private Iterable<Integer> getUnoccupiedAdjacentNodes(MoveGenerationBoard board, Player player) {
+        int location = player.location();
+        var graph = board.getGraph();
+        Set<Integer> adjacent = graph.adjacentNodes(location);
+        Set<Integer> unoccupied = new HashSet<>();
+        for (int adjacentLocation : adjacent) {
+            if (!locationOccupiedByDetective(board, adjacentLocation))
+                unoccupied.add(adjacentLocation);
+        }
+        return unoccupied;
+    }
+
+    private boolean locationOccupiedByDetective(MoveGenerationBoard board, int location) {
+        return board.getDetectiveLocations().contains(location);
+    }
+
+    private Set<ScotlandYard.Transport> getTransports(MoveGenerationBoard board, Player player, int destination) {
+        int location = player.location();
+        var graph = board.getGraph();
+        Optional<ImmutableSet<ScotlandYard.Transport>> mTransport = graph.edgeValue(location, destination);
+        if (mTransport.isPresent())
+            return mTransport.get();
+        else
+            return Set.of();
+    }
+
+    private Move.SingleMove createSingleMove(Player player, ScotlandYard.Ticket ticket, int destination) {
+        return new Move.SingleMove(player.piece(), player.location(), ticket, destination);
+    }
+
+    private Move.SingleMove createSingleMoveWithSecretTicket(Player player, int destination) {
+        return new Move.SingleMove(player.piece(), player.location(), ScotlandYard.Ticket.SECRET, destination);
+    }
+
+    private Set<Move> generateMrXMoves(MoveGenerationBoard board, Player player) {
+        if (player.isDetective())
+            throw new IllegalArgumentException();
+        Set<Move> singleMoves = generateSingleMoves(board, player);
+        Set<Move> doubleMoves = generateDoubleMoves(board, player);
+        Set<Move> moves = singleMoves;
+        moves.addAll(doubleMoves);
+        return moves;
+    }
+
+    private Set<Move> generateDoubleMoves(MoveGenerationBoard board, Player player) {
+        Set<Move> singleMoves = generateSingleMoves(board, player);
+        Set<Move> moves = new HashSet<>();
+        for (Move firstMove : singleMoves) {
+            Set<Move> doubleMoves = createDoubleMoves(board, player, (Move.SingleMove) firstMove);
+            moves.addAll(doubleMoves);
+        }
+        return moves;
+    }
+
+
+    private Set<Move> createDoubleMoves(MoveGenerationBoard board, Player player, Move.SingleMove firstMove) {
+        Player newPlayer = applyMove(player, firstMove);
+        Set<Move> secondMoves = generateSingleMoves(board, newPlayer);
+        Set<Move> doubleMoves = new HashSet<>();
+        for (Move secondMove : secondMoves) {
+            Move.DoubleMove doubleMove = createDoubleMove(player, firstMove, (Move.SingleMove) secondMove);
+            doubleMoves.add(doubleMove);
+        }
+        return doubleMoves;
+    }
+
+    private Player applyMove(Player player, Move move) {
+        return MoveApplyFactory.getInstance().applyMove(player, move);
+    }
+
+    private Move.DoubleMove createDoubleMove(Player player, Move.SingleMove firstMove, Move.SingleMove secondMove) {
+        return new Move.DoubleMove(
+                player.piece(),
+                player.location(),
+                firstMove.ticket,
+                firstMove.destination,
+                secondMove.ticket,
+                secondMove.destination
+        );
     }
 }
