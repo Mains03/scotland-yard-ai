9da933bda51a4f864165f1bf560f9c69d5805d92
Mains03 <samsonsmain@gmail.com>
2022-04-19 17:34:17 +0100

    Remove code duplication

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/minimumDistanceStrategy/minimumDistance/BreadthFirstSearch.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/minimumDistanceStrategy/minimumDistance/BreadthFirstSearch.java
@@ -3,6 +3,8 @@ package uk.ac.bris.cs.scotlandyard.ui.ai.minimumDistanceStrategy.minimumDistance
 import com.google.common.collect.ImmutableSet;
 import com.google.common.graph.ImmutableValueGraph;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard;
+import uk.ac.bris.cs.scotlandyard.ui.ai.minimumDistanceAlgorithm.MinDistStrategy;
+import uk.ac.bris.cs.scotlandyard.ui.ai.minimumDistanceAlgorithm.SimpleBFS;
 import uk.ac.bris.cs.scotlandyard.ui.ai.minimumDistanceStrategy.aiPlayer.AiPlayer;
 
 import java.util.*;
@@ -25,35 +27,7 @@ public class BreadthFirstSearch implements MinimumDistance {
     public int minimumDistance(AiPlayer a, AiPlayer b) {
         int source = a.getLocation();
         int destination = b.getLocation();
-        // Creates a que with the source/player a location as first element
-        Queue<Integer> priorityQueue = new ArrayDeque<>();
-        priorityQueue.add(source);
-        // Creates array of ints with space for each node in graph
-        // Set each int to -1, source has distance 0 from itself
-        int distance[] = new int[graph.nodes().size()+1];
-        for (int i=0; i<distance.length; ++i)
-            distance[i] = -1;
-        distance[source] = 0;
-        while (!priorityQueue.isEmpty()) {
-            // pop first node
-            int node = priorityQueue.poll();
-            // if destination, return its distance, if not, look at adjacent nodes
-            if (node == destination)
-                return distance[node];
-            /*
-            If adjacent node visited, no change, if not visited adds it to queue
-            and stores distance (distance to current node + 1)
-            as new nodes are added to end of queue, order of queue is:
-            nodes with dist 1, nodes with dist 2, etc. so always finds minimum distance
-            */
-            for (int adjacentNode : graph.adjacentNodes(node)) {
-                if (distance[adjacentNode] < 0) {
-                    priorityQueue.add(adjacentNode);
-                    distance[adjacentNode] = distance[node] + 1;
-                }
-            }
-        }
-        // If no route found, has infinite distance
-        return POSITIVE_INFINITY;
+        MinDistStrategy strategy = new SimpleBFS(graph, source, destination);
+        return strategy.getMinimumDistance();
     }
 }
