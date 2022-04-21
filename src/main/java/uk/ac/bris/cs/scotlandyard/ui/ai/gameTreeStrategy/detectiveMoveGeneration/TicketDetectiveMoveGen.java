c6f667ed6350eea79f5fa93fc45d889a1f6b2626
DarkFluffyKnight <gitHub@navdeep.me.uk>
2022-04-21 15:21:26 +0100

    Made moveDetectives

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/gameTreeStrategy/detectiveMoveGeneration/TicketDetectiveMoveGen.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/gameTreeStrategy/detectiveMoveGeneration/TicketDetectiveMoveGen.java
@@ -1,12 +1,39 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai.gameTreeStrategy.detectiveMoveGeneration;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.common.graph.ImmutableValueGraph;
+import uk.ac.bris.cs.scotlandyard.model.Move;
+import uk.ac.bris.cs.scotlandyard.model.Player;
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard;
 import uk.ac.bris.cs.scotlandyard.ui.ai.adapters.aiBoard.AiBoard;
 
-import java.util.Set;
+import java.util.*;
 
 public class TicketDetectiveMoveGen implements DetectiveMoveGeneration{
+    private ArrayList<ArrayList<Move>> moves;
+    private ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph;
+
     @Override
     public Set<AiBoard> moveDetectives(AiBoard aiBoard) {
-        return null;
+        ArrayList<Player> detectives = new ArrayList<>(aiBoard.getDetectives());
+        ArrayList<Move> pastMoves = new ArrayList<>(detectives.size());
+        graph = aiBoard.getGraph();
+        moves.clear();
+        generateCombinations(pastMoves, detectives);
+        return makeAiBoards();
+    }
+
+
+    private void generateCombinations(ArrayList<Move> pastMoves, List<Player> detectives) {
+
+        if (detectives.isEmpty()) {
+            //add final moves
+        } else {
+            // Generate moves
+        }
+    }
+
+    private Set<AiBoard> makeAiBoards() {
+        for (ArrayList<Move> moveSet: moves)
     }
 }
