d36f4c5457d788c146b778263f886e0fa3e884ee
Mains03 <samsonsmain@gmail.com>
2022-04-21 19:17:54 +0100

    Create AlphaBetaStrategy

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/gameTreeStrategy/alphaBeta/AlphaBetaStrategy.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/gameTreeStrategy/alphaBeta/AlphaBetaStrategy.java
@@ -1,23 +1,24 @@
-package uk.ac.bris.cs.scotlandyard.ui.ai.gameTreeStrategy.minimax;
+package uk.ac.bris.cs.scotlandyard.ui.ai.gameTreeStrategy.alphaBeta;
 
 import uk.ac.bris.cs.scotlandyard.model.Board;
 import uk.ac.bris.cs.scotlandyard.model.Move;
 import uk.ac.bris.cs.scotlandyard.ui.ai.BestMoveStrategy;
 import uk.ac.bris.cs.scotlandyard.ui.ai.gameTreeStrategy.GameTree;
-import uk.ac.bris.cs.scotlandyard.ui.ai.gameTreeStrategy.gameTreeStructures.GameTreeNode;
 import uk.ac.bris.cs.scotlandyard.ui.ai.gameTreeStrategy.GameTreeVisitor;
+import uk.ac.bris.cs.scotlandyard.ui.ai.gameTreeStrategy.gameTreeStructures.GameTreeNode;
 import uk.ac.bris.cs.scotlandyard.ui.ai.staticPositionEvaluationStrategy.StaticPosEvalStrategy;
 
 import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Optional;
 
-public class MinimaxStrategy implements BestMoveStrategy {
+public class AlphaBetaStrategy implements BestMoveStrategy {
+    private static final int POSITIVE_INFINITY =  10000000;
     private static final int NEGATIVE_INFINITY = -10000000;
 
     private final StaticPosEvalStrategy strategy;
 
-    public MinimaxStrategy(StaticPosEvalStrategy strategy) {
+    public AlphaBetaStrategy(StaticPosEvalStrategy strategy) {
         this.strategy = Objects.requireNonNull(strategy);
     }
 
@@ -26,8 +27,11 @@ public class MinimaxStrategy implements BestMoveStrategy {
         GameTree gameTree = new GameTree(board);
         Move bestMove = null;
         int bestMoveEval = NEGATIVE_INFINITY;
+        int alpha = NEGATIVE_INFINITY;
+        int beta = POSITIVE_INFINITY;
         for (GameTreeNode node : gameTree.getGameTreeNodes()) {
-            int nodeEvaluation = evaluateNode(node);
+            int nodeEvaluation = evaluateNode(node, alpha, beta);
+            alpha = Math.max(alpha, nodeEvaluation);
             if (nodeEvaluation > bestMoveEval) {
                 bestMove = getMrXMove(node);
                 bestMoveEval = nodeEvaluation;
@@ -38,10 +42,10 @@ public class MinimaxStrategy implements BestMoveStrategy {
         return bestMove;
     }
 
-    private int evaluateNode(GameTreeNode node) {
+    private int evaluateNode(GameTreeNode node, int alpha, int beta) {
         // MrX made move so detectives to move
         boolean maximise = false;
-        GameTreeVisitor visitor = new MinimaxVisitor(maximise, strategy);
+        GameTreeVisitor visitor = new AlphaBetaVisitor(maximise, alpha, beta, strategy);
         return node.accept(visitor);
     }
 
---
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/gameTreeStrategy/alphaBeta/AlphaBetaVisitor.java
@@ -0,0 +1,86 @@
+package uk.ac.bris.cs.scotlandyard.ui.ai.gameTreeStrategy.alphaBeta;
+
+import uk.ac.bris.cs.scotlandyard.ui.ai.adapters.aiBoard.AiBoard;
+import uk.ac.bris.cs.scotlandyard.ui.ai.gameTreeStrategy.GameTreeVisitor;
+import uk.ac.bris.cs.scotlandyard.ui.ai.gameTreeStrategy.gameTreeStructures.GameTreeNode;
+import uk.ac.bris.cs.scotlandyard.ui.ai.gameTreeStrategy.gameTreeStructures.InnerNode;
+import uk.ac.bris.cs.scotlandyard.ui.ai.gameTreeStrategy.gameTreeStructures.LeafNode;
+import uk.ac.bris.cs.scotlandyard.ui.ai.staticPositionEvaluationStrategy.StaticPosEvalStrategy;
+
+import java.util.Iterator;
+import java.util.Objects;
+
+public class AlphaBetaVisitor implements GameTreeVisitor {
+    private static final int POSITIVE_INFINITY =  10000000;
+    private static final int NEGATIVE_INFINITY = -10000000;
+
+    private final boolean maximise;
+
+    private final int alpha;
+    private final int beta;
+
+    private final StaticPosEvalStrategy evalStrategy;
+
+    public AlphaBetaVisitor(boolean maximise, int alpha, int beta, StaticPosEvalStrategy evalStrategy) {
+        this.maximise = maximise;
+        this.alpha = alpha;
+        this.beta = beta;
+        this.evalStrategy = Objects.requireNonNull(evalStrategy);
+    }
+
+    @Override
+    public int visit(InnerNode node) {
+        int evaluation;
+        if (maximise)
+            evaluation = maximiseEvaluation(node);
+        else
+            evaluation = minimiseEvaluation(node);
+        return evaluation;
+    }
+
+    private int maximiseEvaluation(InnerNode node) {
+        int evaluation = NEGATIVE_INFINITY;
+        int alpha = this.alpha;
+        Iterator<GameTreeNode> iterator = node.getChildren().iterator();
+        boolean prune = false;
+        while (iterator.hasNext() && (!prune)) {
+            GameTreeNode child = iterator.next();
+            // this node maximises so child minimises
+            boolean maximise = false;
+            AlphaBetaVisitor visitor = new AlphaBetaVisitor(maximise, alpha, beta, evalStrategy);
+            int childEvaluation = child.accept(visitor);
+            alpha = Math.max(alpha, childEvaluation);
+            if (alpha >= beta)
+                prune = true;
+            else
+                evaluation = Math.max(evaluation, childEvaluation);
+        }
+        return evaluation;
+    }
+
+    private int minimiseEvaluation(InnerNode node) {
+        int evaluation = POSITIVE_INFINITY;
+        int beta = this.beta;
+        Iterator<GameTreeNode> iterator = node.getChildren().iterator();
+        boolean prune = false;
+        while (iterator.hasNext() && (!prune)) {
+            GameTreeNode child = iterator.next();
+            // this node minimises so child maximises
+            boolean maximise = true;
+            AlphaBetaVisitor visitor = new AlphaBetaVisitor(maximise, alpha, beta, evalStrategy);
+            int childEvaluation = child.accept(visitor);
+            beta = Math.min(beta, childEvaluation);
+            if (beta <= alpha)
+                prune = true;
+            else
+                evaluation = Math.max(evaluation, childEvaluation);
+        }
+        return evaluation;
+    }
+
+    @Override
+    public int visit(LeafNode node) {
+        AiBoard board = node.getBoard();
+        return evalStrategy.evaluate(board);
+    }
+}
