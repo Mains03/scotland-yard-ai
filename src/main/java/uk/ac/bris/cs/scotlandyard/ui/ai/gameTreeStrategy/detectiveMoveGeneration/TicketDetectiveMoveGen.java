76ed0da5e6ca94058cbd3fff13025b3dfd121f56
DarkFluffyKnight <gitHub@navdeep.me.uk>
2022-04-21 14:15:58 +0100

    Created TicketDetectiveMoveGen class

---
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/gameTreeStrategy/detectiveMoveGeneration/TicketDetectiveMoveGen.java
@@ -0,0 +1,12 @@
+package uk.ac.bris.cs.scotlandyard.ui.ai.gameTreeStrategy.detectiveMoveGeneration;
+
+import uk.ac.bris.cs.scotlandyard.ui.ai.adapters.aiBoard.AiBoard;
+
+import java.util.Set;
+
+public class TicketDetectiveMoveGen implements DetectiveMoveGeneration{
+    @Override
+    public Set<AiBoard> moveDetectives(AiBoard aiBoard) {
+        return null;
+    }
+}
